3ac17090fe305c87a2decc21155af7e739c3168c
TimGilm <tgilmutdinov@mail.ru>
2023-11-05 11:35:01 +0500

    main modified

--- main.go
+++ main.go
@@ -9,10 +9,10 @@ package main
 
 import "fmt"
 
-func f(x, y int, a func(x, y int) int) int {
+func f(x, y int, a func(x, y int) int) {
 	fmt.Println(a(x, y))
-	return a(x, y)
 }
+
 func main() {
 	defer f(2, 3, func(x, y int) int { return x + y })
 	defer f(2, 3, func(x, y int) int { return x - y })
